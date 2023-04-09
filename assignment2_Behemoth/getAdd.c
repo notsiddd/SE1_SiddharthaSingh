379db5dc5b1b2a3dd526096098b2f30c0de49e12
notsiddd <86549996+notsiddd@users.noreply.github.com>
2023-04-09 22:30:16 +0530

    Create getAdd.c

---
+++ assignment2_Behemoth/getAdd.c
@@ -0,0 +1,12 @@
+//Function to get the address of the environment variable that we are tracing.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    const char* ptr = getenv(argv[1]);
+    ptr += (strlen(argv[0]) - strlen(argv[2]))*2;
+    printf("%x\n", ptr);
+    return 0;
+}
